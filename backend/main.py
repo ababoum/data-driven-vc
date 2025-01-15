c8cb0ade9a7e1ba72a39db74d5560347a03d436e
iakov <yashahelfi2012@gmail.com>
2025-01-15 23:56:49 +0200

    add CI draft

--- backend/main.py
+++ backend/main.py
@@ -35,7 +35,7 @@ app = FastAPI(title="Data Driven VC API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
