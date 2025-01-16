a9fec26c5c6c84d7e5d3cd461ae25ea2b309fa3f
Akram Abd-Ali <aabd-ali@partechpartners.com>
2025-01-16 14:34:22 +0100

    feat: add genation of short tech summary

---
+++ backend/qualitative/short_tech_summary.py
@@ -0,0 +1,69 @@
+from openai import OpenAI
+import json
+import os
+
+openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def wrap_triple_quotes(text: str):
+    return f"```\n{text}\n```"
+
+def generate_company_tech_summary(company = None, webpages: dict = None, domain: str = None, main_techs: list = None, specific_techs: list = None):
+    if not domain:
+        raise Exception("Please provide a domain")
+
+    if not company:
+        raise Exception("Please provide a company object")
+
+    if not webpages:
+        raise Exception("Please provide a webpages object")
+
+    relevant_pages = extract_relevant_pages_url(webpages.keys())
+
+    company_description_prompt = ''
+    if company.get('description'):
+        company_description_prompt = f"company description:\n{wrap_triple_quotes(company.get('description'))}"
+
+    webpages_prompt = ''
+    if relevant_pages and len(relevant_pages) > 0:
+        for page in relevant_pages:
+            webpage_content = webpages.get(page)
+            webpages_prompt += f"- {page}:\n{wrap_triple_quotes(webpage_content)}\n"
+        webpages_prompt = f"web pages:\n{webpages_prompt}"
+
+    messages = [
+        {"role": "developer", "content": "You are a VC analyst, your role is to investigate and rationalize an investment decision on a company. You will be provided the scraped data of multiple pages of the company website. You may also receive list of main technologies and/or specific technologies that many other vc analysts may not understand. You will be also provided with the company basic description. Your task is to generate a short summary of the company for the non technical VC what is the company about, what may be so special about the used technologies if relevant or uncommon, its business model."},
+        {"role": "user", "content": f"""
+
+{company_description_prompt}
+{webpages_prompt}
+{'main technologies: ' + ', '.join(main_techs) if main_techs else ''}
+{'specific technologies: ' + ', '.join(main_techs) if main_techs else ''}
+"""}
+    ]
+
+    response = openai_client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=messages,
+        response_format={
+            "type": "json_schema",
+            "json_schema": {
+                "name": "data",
+                "schema": {
+                    "type": "object",
+                    "properties": {
+                        "summary": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        }
+    )
+
+    completion = response.choices[0].message.content
+    try:
+        parsed_completion = json.loads(completion)
+        return parsed_completion.get('summary')
+    except Exception as e:
+        print(e)
+        return None
